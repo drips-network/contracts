b4fdb52cfb8c87b59b623531b31367db7e54ae42
CodeSandwich <igor.zuk@protonmail.com>
2021-10-06 09:02:59 +0200

    Drop NFT pool

--- src/test/User.t.sol
+++ src/test/User.t.sol
@@ -3,9 +3,7 @@
 pragma solidity ^0.8.7;
 
 import {EthPool, Pool} from "../EthPool.sol";
-import {ERC20Pool, ReceiverWeight, IDai} from "../DaiPool.sol";
-import {NFTPool} from "../NFTPool.sol";
-import {IERC721} from "openzeppelin-contracts/token/ERC721/IERC721.sol";
+import {ERC20Pool, ReceiverWeight} from "../DaiPool.sol";
 
 abstract contract PoolUser {
     function getPool() internal view virtual returns (Pool);
@@ -150,105 +148,5 @@ contract EthPoolUser is PoolUser {
                 amtPerSec,
                 updatedReceivers
             );
-    }
-}
-
-contract NFTPoolUser is PoolUser {
-    NFTPool internal immutable pool;
-    IDai internal dai;
-
-    constructor(NFTPool pool_, IDai dai_) {
-        pool = pool_;
-        dai = dai_;
-    }
-
-    function getPool() internal view override returns (Pool) {
-        return Pool(pool);
-    }
-
-    function balance() public view override returns (uint256) {
-        return pool.erc20().balanceOf(address(this));
-    }
-
-    function updateSender(
-        uint128 toppedUp,
-        uint128 withdrawAmt,
-        uint128 amtPerSec,
-        ReceiverWeight[] calldata updatedReceivers
-    ) public override returns (uint128 withdrawn) {
-        pool.erc20().approve(address(pool), toppedUp);
-        return pool.updateSender(toppedUp, withdrawAmt, amtPerSec, updatedReceivers);
-    }
-
-    function updateSubSender(
-        uint256 subSenderId,
-        uint128 toppedUp,
-        uint128 withdrawAmt,
-        uint128 amtPerSec,
-        ReceiverWeight[] calldata updatedReceivers
-    ) public override returns (uint128 withdrawn) {
-        pool.erc20().approve(address(pool), toppedUp);
-        return
-            pool.updateSubSender(subSenderId, toppedUp, withdrawAmt, amtPerSec, updatedReceivers);
-    }
-
-    function withdraw(uint256 withdrawAmount) public {
-        pool.updateSender(0, uint128(withdrawAmount), 0, new ReceiverWeight[](0));
-    }
-
-    function withdraw(
-        address nftRegistry,
-        uint256 tokenId,
-        uint256 withdrawAmount
-    ) public {
-        pool.updateSender(
-            nftRegistry,
-            uint128(tokenId),
-            0,
-            uint128(withdrawAmount),
-            0,
-            new ReceiverWeight[](0)
-        );
-    }
-
-    function streamWithAddress(
-        address to,
-        uint256 daiPerSecond,
-        uint256 lockAmount
-    ) public {
-        ReceiverWeight[] memory receivers = new ReceiverWeight[](1);
-        receivers[0] = ReceiverWeight({receiver: to, weight: 1});
-
-        dai.approve(address(pool), type(uint256).max);
-        pool.updateSender(uint128(lockAmount), 0, uint128(daiPerSecond), receivers);
-    }
-
-    function streamWithNFT(
-        address nftRegistry,
-        uint256 tokenId,
-        address to,
-        uint256 daiPerSecond,
-        uint256 lockAmount
-    ) public {
-        ReceiverWeight[] memory receivers = new ReceiverWeight[](1);
-        receivers[0] = ReceiverWeight({receiver: to, weight: 1});
-
-        dai.approve(address(pool), type(uint256).max);
-        pool.updateSender(
-            nftRegistry,
-            uint128(tokenId),
-            uint128(lockAmount),
-            0,
-            uint128(daiPerSecond),
-            receivers
-        );
-    }
-
-    function transferNFT(
-        address nftRegistry,
-        address to,
-        uint256 tokenId
-    ) public {
-        IERC721(nftRegistry).transferFrom(address(this), to, tokenId);
     }
 }
