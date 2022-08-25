1d3ce2823e68fca87acc1e37e41957320480c021
b1shopcael <>
2022-08-25 19:46:24 +0000

    feat(AddressApp): add eip-2612 support

--- src/AddressApp.sol
+++ src/AddressApp.sol
@@ -1,9 +1,14 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.15;
 
-import {DripsHub, DripsReceiver, IERC20, SplitsReceiver} from "./DripsHub.sol";
+import {DripsHub, DripsReceiver, SplitsReceiver} from "./DripsHub.sol";
 import {Upgradeable} from "./Upgradeable.sol";
 import {SafeERC20} from "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
+import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
+import {IERC20Permit as IERC20Permit_} from
+    "openzeppelin-contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+
+interface IERC20Permit is IERC20, IERC20Permit_ {}
 
 /// @notice A DripsHub app implementing address-based user identification.
 /// Each address can use `AddressApp` to control a user ID equal to that address.
@@ -14,6 +19,7 @@ import {SafeERC20} from "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol"
 /// will receive a transfer with funds dripped or split to `0x...B`'s user ID.
 contract AddressApp is Upgradeable {
     using SafeERC20 for IERC20;
+    using SafeERC20 for IERC20Permit;
 
     DripsHub public immutable dripsHub;
     uint32 public immutable appId;
@@ -61,14 +67,41 @@ contract AddressApp is Upgradeable {
     /// @param erc20 The token to use
     /// @param amt The given amount
     function give(uint256 receiver, IERC20 erc20, uint128 amt) public {
-        _transferFromCaller(erc20, amt);
+        _transferFrom(msg.sender, erc20, amt);
+        dripsHub.give(calcUserId(msg.sender), receiver, erc20, amt);
+    }
+
+    /// @notice Gives funds from the user to the receiver with an eip-2612 permit signature.
+    /// The receiver can collect them immediately.
+    /// Transfers the funds to be given from the msg.sender's wallet to the drips hub contract.
+    /// @param receiver The receiver.
+    /// @param erc20 The token to use.
+    /// @param amt The given amount.
+    /// @param user The user address.
+    /// @param deadline A deadline for the permit.
+    /// @param v secp256k1 signature
+    /// @param r secp256k1 signature
+    /// @param s secp256k1 signature
+    function give(
+        uint256 receiver,
+        IERC20Permit erc20,
+        uint128 amt,
+        address user,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    )
+        public
+    {
+        _transferFromPermit(user, erc20, amt, deadline, v, r, s);
         dripsHub.give(calcUserId(msg.sender), receiver, erc20, amt);
     }
 
     /// @notice Sets the msg.sender's drips configuration.
     /// Transfers funds between the msg.sender's wallet and the drips hub contract
     /// to fulfill the change of the drips balance.
-    /// @param erc20 The token to use
+    /// @param erc20 The token to use.
     /// @param currReceivers The list of the drips receivers set in the last drips update
     /// of the sender.
     /// If this is the first update, pass an empty array.
@@ -88,7 +121,7 @@ contract AddressApp is Upgradeable {
         returns (uint128 newBalance, int128 realBalanceDelta)
     {
         if (balanceDelta > 0) {
-            _transferFromCaller(erc20, uint128(balanceDelta));
+            _transferFrom(msg.sender, erc20, uint128(balanceDelta));
         }
         (newBalance, realBalanceDelta) =
             dripsHub.setDrips(calcUserId(msg.sender), erc20, currReceivers, balanceDelta, newReceivers);
@@ -97,17 +130,74 @@ contract AddressApp is Upgradeable {
         }
     }
 
+    /// @notice Sets the user's drips configuration with an eip-2612 permit signature.
+    /// Transfers funds between the msg.sender's wallet and the drips hub contract
+    /// to fulfill the change of the drips balance.
+    /// @param erc20 The token to use.
+    /// @param currReceivers The list of the drips receivers set in the last drips update
+    /// of the sender.
+    /// If this is the first update, pass an empty array.
+    /// @param balanceDelta The drips balance change to be applied.
+    /// Positive to add funds to the drips balance, negative to remove them.
+    /// @param newReceivers The list of the drips receivers of the sender to be set.
+    /// Must be sorted by the receivers' addresses, deduplicated and without 0 amtPerSecs.
+    /// @param user The user address.
+    /// @param deadline A deadline for the permit.
+    /// @param v secp256k1 signature
+    /// @param r secp256k1 signature
+    /// @param s secp256k1 signature
+    /// @return newBalance The new drips balance of the sender.
+    /// @return realBalanceDelta The actually applied drips balance change.
+    function setDrips(
+        IERC20Permit erc20,
+        DripsReceiver[] calldata currReceivers,
+        int128 balanceDelta,
+        DripsReceiver[] calldata newReceivers,
+        address user,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    )
+        public
+        returns (uint128 newBalance, int128 realBalanceDelta)
+    {
+        if (balanceDelta > 0) {
+            _transferFromPermit(user, erc20, uint128(balanceDelta), deadline, v, r, s);
+        }
+        (newBalance, realBalanceDelta) =
+            dripsHub.setDrips(calcUserId(msg.sender), erc20, currReceivers, balanceDelta, newReceivers);
+        if (realBalanceDelta < 0) {
+            erc20.safeTransfer(msg.sender, uint128(-realBalanceDelta));
+        }
+    }
     /// @notice Sets msg.sender's splits configuration.
     /// @param receivers The list of the user's splits receivers to be set.
     /// Must be sorted by the splits receivers' addresses, deduplicated and without 0 weights.
     /// Each splits receiver will be getting `weight / TOTAL_SPLITS_WEIGHT`
     /// share of the funds collected by the user.
+
     function setSplits(SplitsReceiver[] calldata receivers) public {
         dripsHub.setSplits(calcUserId(msg.sender), receivers);
     }
 
-    function _transferFromCaller(IERC20 erc20, uint128 amt) internal {
-        erc20.safeTransferFrom(msg.sender, address(this), amt);
+    function _transferFromPermit(
+        address user,
+        IERC20Permit erc20,
+        uint128 amt,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    )
+        internal
+    {
+        erc20.safePermit(user, address(this), uint256(amt), deadline, v, r, s);
+        _transferFrom(user, erc20, amt);
+    }
+
+    function _transferFrom(address user, IERC20 erc20, uint128 amt) internal {
+        erc20.safeTransferFrom(user, address(this), amt);
         address reserve = address(dripsHub.reserve());
         // Approval is done only on the first usage of the ERC-20 token in the reserve by the app
         if (erc20.allowance(address(this), reserve) == 0) {
