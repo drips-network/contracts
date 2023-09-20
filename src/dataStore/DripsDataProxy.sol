394a634f4600a049cf31105847cbd6c78e22fead
CodeSandwich <igor.zuk@protonmail.com>
2023-09-20 13:26:21 +0200

    Add DripsDataProxy (#320)

---
+++ src/dataStore/DripsDataProxy.sol
@@ -0,0 +1,168 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.20;
+
+import {DripsDataStore} from "./DripsDataStore.sol";
+import {Drips, StreamReceiver, IERC20, SplitsReceiver} from "../Drips.sol";
+import {Managed} from "../Managed.sol";
+
+/// @notice A data proxy for `Drips`.
+/// Large parameters aren't passed in calldata, but instead their hashes are accepted,
+/// and the actual data is loaded from `DripsDataStore`.
+/// The data must be explicitly stored in `DripsDataStore` before its usage.
+///
+/// In some cases using the proxy is easier and cheaper than calling the driver directly.
+/// For example multisigs and governance contracts often need the whole calldata
+/// provided as a binary blob, which then sometimes needs to be stored on-chain.
+/// It's easier to build calldata consisting of a flat list of scalar parameters,
+/// and calldata is much smaller when large lists are substituted with their hashes.
+contract DripsDataProxy is Managed {
+    /// @notice The Drips contract used by this proxy.
+    Drips public immutable drips;
+    /// @notice The DripsDataStore contract used by this proxy.
+    DripsDataStore public immutable dripsDataStore;
+
+    /// @param drips_ The Drips contract to use.
+    /// @param dripsDataStore_ The DripsDataStore contract to use.
+    constructor(Drips drips_, DripsDataStore dripsDataStore_) {
+        drips = drips_;
+        dripsDataStore = dripsDataStore_;
+    }
+
+    /// @notice Receive streams from the currently running cycle from a single sender.
+    /// It doesn't receive streams from the finished cycles, to do that use `receiveStreams`.
+    /// Squeezed funds won't be received in the next calls to `squeezeStreams` or `receiveStreams`.
+    /// Only funds streamed before `block.timestamp` can be squeezed.
+    /// @param accountId The ID of the account receiving streams to squeeze funds for.
+    /// @param erc20 The used ERC-20 token.
+    /// It must preserve amounts, so if some amount of tokens is transferred to
+    /// an address, then later the same amount must be transferable from that address.
+    /// Tokens which rebase the holders' balances, collect taxes on transfers,
+    /// or impose any restrictions on holding or transferring tokens are not supported.
+    /// If you use such tokens in the protocol, they can get stuck or lost.
+    /// @param senderId The ID of the streaming account to squeeze funds from.
+    /// @param historyStartHash The sender's history hash that was valid right before
+    /// they set up the sequence of configurations described by `streamsHistoryHash`.
+    /// @param streamsHistoryHash The hash of the sequence of the sender's streams configurations,
+    /// the actual list must be stored in DripsDataStore.
+    /// It can start at an arbitrary past configuration, but must describe all the configurations
+    /// which have been used since then including the current one, in the chronological order.
+    /// Only streams described by `streamsHistoryHash` will be squeezed.
+    /// If `streamsHistoryHash` entries have no receivers, they won't be squeezed.
+    /// @return amt The squeezed amount.
+    function squeezeStreams(
+        uint256 accountId,
+        IERC20 erc20,
+        uint256 senderId,
+        bytes32 historyStartHash,
+        bytes32 streamsHistoryHash
+    ) public whenNotPaused returns (uint128 amt) {
+        return drips.squeezeStreams(
+            accountId,
+            erc20,
+            senderId,
+            historyStartHash,
+            dripsDataStore.loadStreamsHistory(streamsHistoryHash)
+        );
+    }
+
+    /// @notice Calculate effects of calling `squeezeStreams` with the given parameters.
+    /// See its documentation for more details.
+    /// @param accountId The ID of the account receiving streams to squeeze funds for.
+    /// @param erc20 The used ERC-20 token.
+    /// It must preserve amounts, so if some amount of tokens is transferred to
+    /// an address, then later the same amount must be transferable from that address.
+    /// Tokens which rebase the holders' balances, collect taxes on transfers,
+    /// or impose any restrictions on holding or transferring tokens are not supported.
+    /// If you use such tokens in the protocol, they can get stuck or lost.
+    /// @param senderId The ID of the streaming account to squeeze funds from.
+    /// @param historyStartHash The sender's history hash that was valid right before
+    /// they set up the sequence of configurations described by `streamsHistoryHash`.
+    /// @param streamsHistoryHash The hash of the sequence of the sender's streams configurations,
+    /// the actual list must be stored in DripsDataStore.
+    /// @return amt The squeezed amount.
+    function squeezeStreamsResult(
+        uint256 accountId,
+        IERC20 erc20,
+        uint256 senderId,
+        bytes32 historyStartHash,
+        bytes32 streamsHistoryHash
+    ) public view returns (uint128 amt) {
+        return drips.squeezeStreamsResult(
+            accountId,
+            erc20,
+            senderId,
+            historyStartHash,
+            dripsDataStore.loadStreamsHistory(streamsHistoryHash)
+        );
+    }
+
+    /// @notice Calculate the result of splitting an amount using the current splits configuration.
+    /// The currently set list of splits receivers must be stored in DripsDataStore.
+    /// @param accountId The account ID.
+    /// @param amount The amount being split.
+    /// @return collectableAmt The amount made collectable for the account
+    /// on top of what was collectable before.
+    /// @return splitAmt The amount split to the account's splits receivers
+    function splitResult(uint256 accountId, uint128 amount)
+        public
+        view
+        returns (uint128 collectableAmt, uint128 splitAmt)
+    {
+        bytes32 splitsHash = drips.splitsHash(accountId);
+        // slither-disable-next-line unused-return
+        return drips.splitResult(accountId, dripsDataStore.loadSplits(splitsHash), amount);
+    }
+
+    /// @notice Splits the account's splittable funds among receivers.
+    /// The entire splittable balance of the given ERC-20 token is split.
+    /// All split funds are split using the current splits configuration.
+    /// Because the account can update their splits configuration at any time,
+    /// it is possible that calling this function will be frontrun,
+    /// and all the splittable funds will become splittable only using the new configuration.
+    /// The account must be trusted with how funds sent to them will be splits,
+    /// in the end they can do with their funds whatever they want by changing the configuration.
+    /// The currently set list of splits receivers must be stored in DripsDataStore.
+    /// @param accountId The account ID.
+    /// @param erc20 The used ERC-20 token.
+    /// It must preserve amounts, so if some amount of tokens is transferred to
+    /// an address, then later the same amount must be transferable from that address.
+    /// Tokens which rebase the holders' balances, collect taxes on transfers,
+    /// or impose any restrictions on holding or transferring tokens are not supported.
+    /// If you use such tokens in the protocol, they can get stuck or lost.
+    /// @return collectableAmt The amount made collectable for the account
+    /// on top of what was collectable before.
+    /// @return splitAmt The amount split to the account's splits receivers
+    function split(uint256 accountId, IERC20 erc20)
+        public
+        whenNotPaused
+        returns (uint128 collectableAmt, uint128 splitAmt)
+    {
+        bytes32 splitsHash = drips.splitsHash(accountId);
+        // slither-disable-next-line unused-return
+        return drips.split(accountId, erc20, dripsDataStore.loadSplits(splitsHash));
+    }
+
+    /// @notice The account's streams balance at the given timestamp.
+    /// The currently set list of stream receivers must be stored in DripsDataStore.
+    /// @param accountId The account ID.
+    /// @param erc20 The used ERC-20 token.
+    /// It must preserve amounts, so if some amount of tokens is transferred to
+    /// an address, then later the same amount must be transferable from that address.
+    /// Tokens which rebase the holders' balances, collect taxes on transfers,
+    /// or impose any restrictions on holding or transferring tokens are not supported.
+    /// If you use such tokens in the protocol, they can get stuck or lost.
+    /// @param timestamp The timestamps for which balance should be calculated.
+    /// It can't be lower than the timestamp of the last call to `setStreams`.
+    /// If it's bigger than `block.timestamp`, then it's a prediction assuming
+    /// that `setStreams` won't be called before `timestamp`.
+    /// @return balance The account balance on `timestamp`
+    function balanceAt(uint256 accountId, IERC20 erc20, uint32 timestamp)
+        public
+        view
+        returns (uint128 balance)
+    {
+        // slither-disable-next-line unused-return
+        (bytes32 streamsHash,,,,) = drips.streamsState(accountId, erc20);
+        return drips.balanceAt(accountId, erc20, dripsDataStore.loadStreams(streamsHash), timestamp);
+    }
+}
---
+++ test/dataStore/DripsDataProxy.t.sol
@@ -0,0 +1,126 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.20;
+
+import {DripsDataProxy, DripsDataStore} from "src/dataStore/DripsDataProxy.sol";
+import {
+    Drips, StreamConfigImpl, StreamReceiver, StreamsHistory, SplitsReceiver
+} from "src/Drips.sol";
+import {ManagedProxy} from "src/Managed.sol";
+import {Test} from "forge-std/Test.sol";
+import {
+    IERC20,
+    ERC20PresetFixedSupply
+} from "openzeppelin-contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
+
+contract DripsDataProxyTest is Test {
+    Drips internal drips;
+    DripsDataStore internal dripsDataStore;
+    DripsDataProxy internal dataProxy;
+    IERC20 internal erc20;
+
+    address internal driver = address(1);
+    address internal admin = address(2);
+
+    uint256 internal account = 1;
+    uint256 internal receiver = 2;
+
+    function setUp() public {
+        Drips dripsLogic = new Drips(10);
+        drips = Drips(address(new ManagedProxy(dripsLogic, address(this))));
+        drips.registerDriver(driver);
+
+        dripsDataStore = new DripsDataStore();
+        DripsDataProxy dataProxyLogic = new DripsDataProxy(drips, dripsDataStore);
+        dataProxy = DripsDataProxy(address(new ManagedProxy(dataProxyLogic, admin)));
+
+        erc20 = new ERC20PresetFixedSupply("test", "test", type(uint136).max, address(this));
+    }
+
+    function testSqueezeStreams() public {
+        // Start streaming
+        StreamReceiver[] memory streams = new StreamReceiver[](1);
+        streams[0] = StreamReceiver(
+            receiver, StreamConfigImpl.create(0, 1 * drips.AMT_PER_SEC_MULTIPLIER(), 0, 0)
+        );
+        erc20.transfer(address(drips), 2);
+        vm.prank(driver);
+        drips.setStreams(account, erc20, new StreamReceiver[](0), 2, streams, 0, 0);
+
+        // Create history
+        (,, uint32 updateTime,, uint32 maxEnd) = drips.streamsState(account, erc20);
+        StreamsHistory[] memory history = new StreamsHistory[](1);
+        history[0] = StreamsHistory(0, streams, updateTime, maxEnd);
+        bytes32 historyHash = dripsDataStore.storeStreamsHistory(history);
+
+        // Test squeezeStreamsResult
+        skip(1);
+        uint128 amt = dataProxy.squeezeStreamsResult(receiver, erc20, account, 0, historyHash);
+        assertEq(amt, 1, "Invalid squeezable amt");
+
+        // Squeeze
+        amt = dataProxy.squeezeStreams(receiver, erc20, account, 0, historyHash);
+        assertEq(amt, 1, "Invalid squeezed amt");
+        assertEq(drips.splittable(receiver, erc20), 1, "Invalid splittable amt");
+    }
+
+    function testSplit() public {
+        uint32 splitWeight = drips.TOTAL_SPLITS_WEIGHT() / 4;
+        uint128 totalAmt = 8;
+        uint128 splitAmt = 2;
+        uint128 collectableAmt = 6;
+
+        // Set splits
+        SplitsReceiver[] memory splits = new SplitsReceiver[](1);
+        splits[0] = SplitsReceiver(receiver, splitWeight);
+        dripsDataStore.storeSplits(splits);
+        vm.prank(driver);
+        drips.setSplits(account, splits);
+
+        // Test splitResult
+        (uint128 actualCollectableAmt, uint128 actualSplitAmt) =
+            dataProxy.splitResult(account, totalAmt);
+        assertEq(actualCollectableAmt, collectableAmt, "Invalid results collectable amount");
+        assertEq(actualSplitAmt, splitAmt, "Invalid results split amount");
+
+        // Give funds
+        erc20.transfer(address(drips), totalAmt);
+        vm.prank(driver);
+        drips.give(receiver, account, erc20, totalAmt);
+
+        // Split
+        (actualCollectableAmt, actualSplitAmt) = dataProxy.split(account, erc20);
+        assertEq(actualCollectableAmt, collectableAmt, "Invalid collectable amount");
+        assertEq(actualSplitAmt, splitAmt, "Invalid split amount");
+        assertEq(drips.splittable(receiver, erc20), splitAmt, "Invalid actually split");
+        assertEq(drips.collectable(account, erc20), collectableAmt, "Invalid actually collectable");
+    }
+
+    function testBalanceAt() public {
+        StreamReceiver[] memory streams = new StreamReceiver[](1);
+        streams[0] = StreamReceiver(
+            receiver, StreamConfigImpl.create(0, 1 * drips.AMT_PER_SEC_MULTIPLIER(), 0, 0)
+        );
+        dripsDataStore.storeStreams(streams);
+        erc20.transfer(address(drips), 2);
+        vm.prank(driver);
+        drips.setStreams(account, erc20, new StreamReceiver[](0), 2, streams, 0, 0);
+
+        uint256 balanceAt = dataProxy.balanceAt(account, erc20, uint32(block.timestamp + 1));
+        assertEq(balanceAt, 1, "Invalid balance");
+    }
+
+    modifier canBePausedTest() {
+        vm.prank(admin);
+        dataProxy.pause();
+        vm.expectRevert("Contract paused");
+        _;
+    }
+
+    function testSqueezeStreamsCanBePaused() public canBePausedTest {
+        dataProxy.squeezeStreams(account, erc20, account, 0, 0);
+    }
+
+    function testSplitCanBePaused() public canBePausedTest {
+        dataProxy.split(account, erc20);
+    }
+}
