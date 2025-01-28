84e8ad5d4882b8841b617f5b84b565e1e3a35f72
Jason Efstathiou <jason.efst@gmail.com>
2025-01-28 13:40:03 +0100

    ensure build actually fails when it does

--- scripts/deploy-docker.sh
+++ scripts/deploy-docker.sh
@@ -10,8 +10,14 @@ source ./scripts/local-env.sh
 export ETH_RPC_URL=http://localhost:8545
 
 yes | ./scripts/deploy.sh
+if [ $? -ne 0 ]; then
+  exit 1
+fi
 
 yes | forge create --private-key 0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6 lib/openzeppelin-contracts/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol:ERC20PresetFixedSupply --broadcast --constructor-args "Test token" "TEST" 1000 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+if [ $? -ne 0 ]; then
+  exit 1
+fi
 
 anvil_pid=$(ps aux | grep anvil | grep -v grep | awk '{print $2}')
 kill -2 $anvil_pid
