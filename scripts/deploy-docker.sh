2c5a209e2efce5ce9fbbe4ebf303099df62c4e62
Jason Efstathiou <jason.efst@gmail.com>
2025-01-28 13:47:44 +0100

    give the test account in docker image a proper amount of eth

--- scripts/deploy-docker.sh
+++ scripts/deploy-docker.sh
@@ -14,7 +14,7 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-yes | forge create --private-key 0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6 lib/openzeppelin-contracts/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol:ERC20PresetFixedSupply --broadcast --constructor-args "Test token" "TEST" 1000 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+yes | forge create --private-key 0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6 lib/openzeppelin-contracts/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol:ERC20PresetFixedSupply --broadcast --constructor-args "Test token" "TEST" 100000000000000000000 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
 if [ $? -ne 0 ]; then
   exit 1
 fi
--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -9,7 +9,7 @@ printf "🪙 Test ERC 20 is deployed at:\n"
 
 printf "0x700b6A60ce7EaaEA56F065753d8dcB9653dbAD35 \n\n"
 
-printf "🪙 Address 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 has 1000 TEST.\n\n"
+printf "🪙 Address 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 has 100000000000000000000 TEST.\n\n"
 
 printf "Starting testnet...\n\n"
 
