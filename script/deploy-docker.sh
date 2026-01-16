2c5cd74712880ece655e41232bac0c144b3a1044
Igor Żuk <igor.zuk@protonmail.com>
2026-01-16 13:47:29 +0100

    Improve error handling in local execution of deploy-docker.sh

--- script/deploy-docker.sh
+++ script/deploy-docker.sh
@@ -3,19 +3,22 @@ set -euxo pipefail
 
 echo Deploying contracts to anvil...
 
-mkdir anvil-data
-
-mkfifo anvil_output
-
-anvil --dump-state ./anvil-state.json --host 0.0.0.0 > anvil_output 2>&1 &
+rm -f anvil_stdout
+mkfifo anvil_stdout
+anvil --dump-state ./anvil-state.json --host 0.0.0.0 > anvil_stdout &
 anvil_pid=$!
 
-while ! grep -q "Listening on 0.0.0.0:8545" < anvil_output; do
+while ! grep -q "Listening on 0.0.0.0:8545" < anvil_stdout; do
   sleep 1
+  ps -p $anvil_pid > /dev/null
 done
 
-forge script -f localhost:8545 --broadcast --slow --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 script/LocalTestnet.sol:Deploy
+forge script -f localhost:8545 --broadcast --slow \
+  --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 \
+  script/LocalTestnet.sol:Deploy \
+  || SCRIPT_STATUS=$?
 
 kill -2 $anvil_pid
 wait
-rm anvil_output
+rm anvil_stdout
+exit ${SCRIPT_STATUS:-0}
